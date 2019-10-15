7392d2a1513ea80288415822afe2752282445f31
Pedsmarter756 <pedro.fernandez.ar9584@gmail.com>
2019-10-15 16:35:08 +0200

    change running point

--- app.py
+++ app.py
@@ -209,9 +209,7 @@ def contact():
 
 
 
-if __name__ == '__main__':
-    app.run(
-        host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=False)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080,debug=True)
+
 
