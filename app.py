5ef3a2047031f4836ac01a81bf22b43341d0c49d
Pedsmarter756 <pedro.fernandez.ar9584@gmail.com>
2019-10-15 23:04:23 +0200

    port

--- app.py
+++ app.py
@@ -209,7 +209,9 @@ def contact():
 
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080,debug=False)
-
+if __name__ == '__main__':
+    app.run(
+        host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
 
