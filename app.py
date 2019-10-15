9d5febef397855f9b7f7cdd2a899f20f2e2f2498
Pedsmarter756 <pedro.fernandez.ar9584@gmail.com>
2019-10-15 10:28:55 +0200

    add prt and debug to false

--- app.py
+++ app.py
@@ -209,7 +209,9 @@ def contact():
 
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080,debug=True)
-
+if __name__ == '__main__':
+    app.run(
+        host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=False)
 
