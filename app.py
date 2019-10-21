b4c17df0a3bc4d3542a13d5025fbef8aabd211e8
Pedsmarter756 <pedro.fernandez.ar9584@gmail.com>
2019-10-21 10:59:40 +0200

    add email

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def contact():
       flash('All fields are required.')
       return render_template('contact.html', form=form)
     else:
-      msg = Message(form.subject.data, sender='contact@example.com', recipients=['masasimpafra@gmail.com'])
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['frankmasabo55@gmail.com'])
       msg.body = """
       From: %s &lt;%s&gt;
       %s
